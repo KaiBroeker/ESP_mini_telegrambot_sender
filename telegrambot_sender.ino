e329d91870c4ebe9e7bb9e0c30d4f126f1b607bf
Kai Bröker <Kai@broeker.tech>
2020-12-10 22:48:31 +0100

    Create telegrambot_sender.ino
    initial git commit for this code
    
    Uploaded the telegrambot source code I wrote some time ago.
    Maybe helpful for someone who need a telegram bot without using a big lib.

---
+++ telegrambot_sender.ino
@@ -0,0 +1,102 @@
+#include <ESP8266WiFi.h>
+#include <time.h>
+
+
+//wifi settings
+const char *ssid = "[wifi-name]";
+const char *pass = "[wifi-password]";
+
+//connection information
+const char *   host = "api.telegram.org";
+const uint16_t port = 443;
+// /[botstring]/sendMessage?chat_id=[chatid]&text=[message]
+const char *   path = "/[bot263...]/sendMessage?chat_id=[15...]&text=[encoded message]";
+
+//root cert of the cert chain
+static const char digicert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIEADCCAuigAwIBAgIBADANBgkqhkiG9w0BAQUFADBjMQswCQYDVQQGEwJVUzEh
+MB8GA1UEChMYVGhlIEdvIERhZGR5IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBE
+YWRkeSBDbGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA0MDYyOTE3
+MDYyMFoXDTM0MDYyOTE3MDYyMFowYzELMAkGA1UEBhMCVVMxITAfBgNVBAoTGFRo
+ZSBHbyBEYWRkeSBHcm91cCwgSW5jLjExMC8GA1UECxMoR28gRGFkZHkgQ2xhc3Mg
+MiBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASAwDQYJKoZIhvcNAQEBBQADggEN
+ADCCAQgCggEBAN6d1+pXGEmhW+vXX0iG6r7d/+TvZxz0ZWizV3GgXne77ZtJ6XCA
+PVYYYwhv2vLM0D9/AlQiVBDYsoHUwHU9S3/Hd8M+eKsaA7Ugay9qK7HFiH7Eux6w
+wdhFJ2+qN1j3hybX2C32qRe3H3I2TqYXP2WYktsqbl2i/ojgC95/5Y0V4evLOtXi
+EqITLdiOr18SPaAIBQi2XKVlOARFmR6jYGB0xUGlcmIbYsUfb18aQr4CUWWoriMY
+avx4A6lNf4DD+qta/KFApMoZFv6yyO9ecw3ud72a9nmYvLEHZ6IVDd2gWMZEewo+
+YihfukEHU1jPEX44dMX4/7VpkI+EdOqXG68CAQOjgcAwgb0wHQYDVR0OBBYEFNLE
+sNKR1EwRcbNhyz2h/t2oatTjMIGNBgNVHSMEgYUwgYKAFNLEsNKR1EwRcbNhyz2h
+/t2oatTjoWekZTBjMQswCQYDVQQGEwJVUzEhMB8GA1UEChMYVGhlIEdvIERhZGR5
+IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBEYWRkeSBDbGFzcyAyIENlcnRpZmlj
+YXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQAD
+ggEBADJL87LKPpH8EsahB4yOd6AzBhRckB4Y9wimPQoZ+YeAEW5p5JYXMP80kWNy
+OO7MHAGjHZQopDH2esRU1/blMVgDoszOYtuURXO1v0XJJLXVggKtI3lpjbi2Tc7P
+TMozI+gciKqdi0FuFskg5YmezTvacPd+mSYgFFQlq25zheabIZ0KbIIOqPjCDPoQ
+HmyW74cNxA9hi63ugyuV+I6ShHI56yDqg+2DzZduCLzrTia2cyvk0/ZM/iZx4mER
+dEr/VxqHD3VILs9RaRegAhJhldXRQLIQTO7ErBBDpqWeCtWVYpoNz4iCxTIM5Cuf
+ReYNnyicsbkqWletNw+vHX/bvZ8=
+-----END CERTIFICATE-----
+)EOF";
+
+
+void setup() {
+  //connect to wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  BearSSL::WiFiClientSecure client;
+  BearSSLX509List cert(digicert);
+  client.setTrustAnchors(&cert);
+ 
+  //set clock
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    now = time(nullptr);
+  }
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+
+
+
+  // connect to client
+  client.connect(host, port);
+
+  if (client.connected()) {
+    client.write("GET ");
+    client.write(path);
+    client.write(" HTTP/1.0\r\nHost: ");
+    client.write(host);
+    client.write("\r\nUser-Agent: ESP8266\r\n");
+    client.write("\r\n");
+    uint32_t to = millis() + 5000;
+    if (client.connected()) {
+        do {
+        char tmp[32];
+        memset(tmp, 0, 32);
+        int rlen = client.read((uint8_t*)tmp, sizeof(tmp) - 1);
+        yield();
+        if (rlen < 0) {
+            break;
+        }
+        } while (millis() < to);
+    }
+    client.stop();
+  }
+
+  // set the esp to infinit sleep (power saving)
+  // useful if a taster is connected to the reset pin
+  delay(2000);
+  ESP.deepSleep(0);
+}
+
+void loop() {
+  
+}
